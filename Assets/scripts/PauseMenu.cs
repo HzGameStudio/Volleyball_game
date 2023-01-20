4d6d22a12d194c923a1aea3eca30b62da051659e
Ivan Antonov <vanya@jets.kiev.ua>
2023-01-20 20:49:31 +0100

    add pause menu

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("AMOGUS");
+        // nothing here yet because fuck you
+        // change scene to settings scene 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
