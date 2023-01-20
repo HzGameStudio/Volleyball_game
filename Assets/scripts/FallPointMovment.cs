1fd0803d52180d759dc3cd726e6369565ea638cd
Ya2403 <kondrachuk06@gmail.com>
2023-01-20 10:05:14 +0100

    really fixed red point

--- Assets/scripts/FallPointMovment.cs
+++ Assets/scripts/FallPointMovment.cs
@@ -12,7 +12,7 @@ public class FallPointMovment : MonoBehaviour
 
     void Update()
     {
-        if(rb.velocity.y>0 && gameObject.transform.position.x > 0 && flag)
+        if(gameObject.transform.position.x > 0 && flag)
         {
             
             fallPoint.position = GetFallPointPosotion();
@@ -32,22 +32,37 @@ public class FallPointMovment : MonoBehaviour
         velocityY= rb.velocity.y;
         velocityZ= rb.velocity.z;
 
-        float groundY = 21.5f;
+        float groundY = 24.7f + 1.5f;
 
         float x, y,z;
         x = gameObject.transform.position.x;
         y = gameObject.transform.position.y - groundY;
         z = gameObject.transform.position.z;
 
-        float discriminator = velocityY * velocityY + 2 * fallAcseleration * y;
-
-        float flyingTime = 2 * velocityY / fallAcseleration + (velocityY + Mathf.Sqrt(discriminator)) / fallAcseleration;
-
         Vector3 fallPoint;
 
-        fallPoint.y = groundY;
-        fallPoint.x = x + flyingTime * velocityX;
-        fallPoint.z = z + flyingTime* velocityZ;
+        if (velocityY>0)
+        {
+            float discriminator = velocityY * velocityY + 2 * fallAcseleration * y;
+
+            float flyingTime = 2 * velocityY / fallAcseleration + (velocityY + Mathf.Sqrt(discriminator)) / fallAcseleration;
+
+            fallPoint.y = groundY;
+            fallPoint.x = x + flyingTime * velocityX;
+            fallPoint.z = z + flyingTime * velocityZ;
+        }
+        else
+        {
+            float discriminator = velocityY * velocityY + 2 * fallAcseleration * y;
+
+            float flyingTime = (velocityY + Mathf.Sqrt(discriminator)) / fallAcseleration;
+
+            fallPoint.y = groundY;
+            fallPoint.x = x + flyingTime * velocityX;
+            fallPoint.z = z + flyingTime * velocityZ;
+        }
+
+        
 
         //Debug.Log(fallPoint);
 
