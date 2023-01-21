6acd1569a6cd84ff95938975689a9f7604d16592
Ya2403 <kondrachuk06@gmail.com>
2023-01-21 16:12:49 +0100

    system of angle change for bottom hands position

---
+++ Assets/scripts/ChangeBottomPosition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBottomPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform bottomHandPosition;
+    public float step;
+
+    public float minAngle;
+    public float maxAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.active)
+        {
+            Vector3 rotation = bottomHandPosition.localRotation.eulerAngles;
+
+            //Debug.Log(rotation);
+            rotation.x += step * Input.mouseScrollDelta.y;
+            //rotation.x = Mathf.Clamp(rotation.x, -70f, 70f);
+
+            if(rotation.x<=90)
+            {
+                if(rotation.x>=minAngle)
+                {
+                    rotation.x = minAngle;
+                }
+            }
+            else if(rotation.x >=270)
+            {
+                if(rotation.x<maxAngle)
+                {
+                    rotation.x = maxAngle;
+                }
+            }
+            bottomHandPosition.localRotation = Quaternion.Euler(rotation);
+        }
+    }
+}
